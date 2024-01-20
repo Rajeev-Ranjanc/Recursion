fbae9ecfe0c318b1c33aab4430cf13f7d31ccdfe
Rajeev Ranjan <itrajeevranjan@gmail.com>
2024-01-21 00:01:30 +0530

    binary_recur

--- BS_rec.java
+++ BS_rec.java
@@ -4,7 +4,7 @@ binary search using recursion
 public class BS_rec {
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4, 55, 66, 78};
-        int target = 3;
+        int target = 7;
         System.out.println(BinarySearch(nums, target, 0, nums.length - 1));
     }
 
@@ -21,8 +21,8 @@ public class BS_rec {
         }
         //target is smaller than we have to traverse next part of the array
         if(target<nums[mid]){
-            return BinarySearch(nums , target ,mid+1 , end );
+            return BinarySearch(nums , target ,start , mid-1 );
         }
-        return BinarySearch(nums , target , start , mid-1);
+        return BinarySearch(nums , target , mid+1 , end);
     }
 }
